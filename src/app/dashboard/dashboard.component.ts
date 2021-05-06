a815c639a8fe87ccd21d323563c171a885674add
aperez <andres.perez@dreamcodesoft.com-at-290296201161>
2021-05-06 18:11:35 -0500

    changes for dashboard cards

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -9,10 +9,16 @@ import { CardMainDashboard } from '../models/card-main-dashboard';
 export class DashboardComponent implements OnInit {
   public cardsDashboard: Array<CardMainDashboard>;
   public cardsOverview: Array<CardMainDashboard>;
-  FACEBOOK: number = 1;
-  INSTAGRAM: number = 2;
-  TWITTER: number = 3;
-  YOUTUBE: number = 4;
+  FACEBOOK_COLOR: number = 1;
+  INSTAGRAM_COLOR: number = 2;
+  TWITTER_COLOR: number = 3;
+  YOUTUBE_COLOR: number = 4;
+
+  FACEBOOK_ICON:string='../../assets/dashboard/icon-facebook.svg';
+  TWITTER_ICON:string='../../assets/dashboard/icon-twitter.svg';
+  INSTAGRAM_ICON:string='../../assets/dashboard/icon-instagram.svg';
+  YOUTUBE_ICON:string='../../assets/dashboard/icon-youtube.svg';
+
 
   constructor() {}
 
@@ -23,39 +29,39 @@ export class DashboardComponent implements OnInit {
   private fillCards() {
     this.cardsDashboard = new Array<CardMainDashboard>();
     let facebookCard: CardMainDashboard = {
-      socialNetworkIcon: '../../assets/dashboard/icon-facebook.svg',
+      socialNetworkIcon: this.FACEBOOK_ICON,
       socialNetworkUsername: '@nathanf',
       numberOfFollowers: '1987',
       followerType: 'Followers',
       numberOfDays: 12,
-      socialNetworkType: this.FACEBOOK,
+      socialNetworkType: this.FACEBOOK_COLOR,
     };
 
     let twitterCard: CardMainDashboard = {
-      socialNetworkIcon: '../../assets/dashboard/icon-twitter.svg',
+      socialNetworkIcon: this.TWITTER_ICON,
       socialNetworkUsername: '@nathanf',
       numberOfFollowers: '1044',
       followerType: 'Followers',
       numberOfDays: 99,
-      socialNetworkType: this.TWITTER
+      socialNetworkType: this.TWITTER_COLOR
     };
 
     let instagramCard: CardMainDashboard = {
-      socialNetworkIcon: '../../assets/dashboard/icon-instagram.svg',
+      socialNetworkIcon: this.INSTAGRAM_ICON,
       socialNetworkUsername: '@realnathanf',
       numberOfFollowers: '11k',
       followerType: 'Followers',
       numberOfDays: 1099,
-      socialNetworkType: this.INSTAGRAM
+      socialNetworkType: this.INSTAGRAM_COLOR
     };
 
     let youtubeCard: CardMainDashboard = {
-      socialNetworkIcon: '../../assets/dashboard/icon-youtube.svg',
+      socialNetworkIcon: this.YOUTUBE_ICON,
       socialNetworkUsername: 'Nathan F.',
       numberOfFollowers: '8239',
       followerType: 'Subscribers',
       numberOfDays: 144,
-      socialNetworkType: this.YOUTUBE
+      socialNetworkType: this.YOUTUBE_COLOR
     };
 
     this.cardsDashboard.push(facebookCard);
@@ -64,12 +70,12 @@ export class DashboardComponent implements OnInit {
     this.cardsDashboard.push(youtubeCard);
 
     let viewsFacebookCard: CardMainDashboard = {
-      socialNetworkIcon: '../../assets/dashboard/icon-facebook.svg',
+      socialNetworkIcon: this.FACEBOOK_ICON,
       socialNetworkUsername: 'Page Views',
       numberOfFollowers: '87',
       followerType: '',
       numberOfDays: 3,
-      socialNetworkType: this.FACEBOOK
+      socialNetworkType: this.FACEBOOK_COLOR
     };
   }
 }
